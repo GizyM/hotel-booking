01b84704dc9eb6989eac7560fbd054718e53a1c2
Gizy Michel <batblover@yahoo.com>
2026-03-29 18:07:15 -0400

    initialized git repository

---
+++ src/pages/MyBookings.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const MyBookings = () => {
+  return (
+    <div className='py-28 md:pb-35 md:pt-32 px-4 md:px-16 lg:px-24 xl:px-32'>
+
+      <Title title='My Bookings' subTitle='Easily manage your past, current,
+      and upcoming hotel reservations in one place. Plan your trips seamlessly with
+      just a few clicks.' align='left' />
+
+      <div>
+        <div className='hidden md:grid mg:grid-cols-[3fr_2fr_1fr] w-full border-b
+        border-gray-300 font-medium text-base py-3'>
+            <div className='w-1/3'>Hotels</div>
+            <div className='w-1/3'>Date & Timings</div>
+            <div className='w-1/3'>Payment</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MyBookings
